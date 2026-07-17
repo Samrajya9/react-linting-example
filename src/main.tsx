2af71c3be24641fe536d3dd211b9cb9023dcc07b
Samrajya Pratap Rana <phinix0980@gmail.com>
2026-07-17 23:41:22 +0545

    init 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import App from "./app.tsx";
-// import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
